b97ed7695497ed513282bd87cf2edc118b2dedeb
yuuki1293 <yuuki1293@outlook.jp>
2022-11-23 18:08:30 +0900

    feat: Λrrowを依存関係に追加

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val arrow_version: String by project
 
 plugins {
     application
@@ -25,6 +26,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.arrow-kt:arrow-core:$arrow_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
