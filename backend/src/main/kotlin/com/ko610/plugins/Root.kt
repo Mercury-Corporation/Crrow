bbf4c9e2ff7c5a2df7adcbfc183b9f51984be688
yuuki1293 <yuuki1293@outlook.jp>
2022-11-23 18:07:54 +0900

    refactor: rename Routing to Root

--- backend/src/main/kotlin/com/ko610/plugins/Root.kt
+++ backend/src/main/kotlin/com/ko610/plugins/Root.kt
@@ -1,10 +1,8 @@
 package com.ko610.plugins
 
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
@@ -13,7 +11,5 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
-    }
-    routing {
     }
 }
