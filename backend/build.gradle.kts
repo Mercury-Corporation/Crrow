c2f33620bb6782bb3b71a7dcb7c181411815edbc
yuuki1293 <yuuki1293@outlook.jp>
2022-12-03 21:30:44 +0900

    feat(backend): update mainClass to io.ktor.server.netty.EngineMain

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 group = "com.ko610"
 version = "0.0.1"
 application {
-    mainClass.set("com.ko610.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
