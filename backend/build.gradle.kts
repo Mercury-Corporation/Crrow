b3b7b5af184ed9b2f581edffcf7cfa53af1189fa
yuuki1293 <yuuki1293@outlook.jp>
2022-12-03 22:19:18 +0900

    feat(backend): add dependency ktor-server-content-negotiation-jvm

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.arrow-kt:arrow-core:$arrow_version")
