49c12b08f22437a3549675d81938bf379c21144f
yuuki1293 <yuuki1293@outlook.jp>
2022-12-03 21:51:11 +0900

    feat(backend): Application.kt embedded server to EngineMain

--- backend/src/main/kotlin/com/ko610/Application.kt
+++ backend/src/main/kotlin/com/ko610/Application.kt
@@ -1,14 +1,10 @@
 package com.ko610
 
+import com.ko610.plugins.configureRouting
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.ko610.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
