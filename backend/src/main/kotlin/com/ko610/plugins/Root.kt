a762fea306741a5a0988bfb6e1a2b96c7bb0583b
yuuki1293 <yuuki1293@outlook.jp>
2022-12-03 22:21:40 +0900

    feat(backend): install json

--- backend/src/main/kotlin/com/ko610/plugins/Root.kt
+++ backend/src/main/kotlin/com/ko610/plugins/Root.kt
@@ -1,12 +1,16 @@
 package com.ko610.plugins
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
+import io.ktor.server.plugins.contentnegotiation.*
 
 fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        json()
+    }
 
-    // Starting point for a Ktor app:
     routing {
         get("/") {
             call.respondText("Hello World!")
