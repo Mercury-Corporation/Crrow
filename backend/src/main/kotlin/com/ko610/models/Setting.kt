b41e82cfd6cce09b40d2b58700b270ddf72660ae
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 11:54:49 +0900

    feat(backend): add Setting.kt

---
+++ backend/src/main/kotlin/com/ko610/models/Setting.kt
@@ -0,0 +1,13 @@
+package com.ko610.models
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.javatime.date
+
+object Setting : Table() {
+    val userId = reference("User_id", User)
+    val nickName = varchar("nickname", 20)
+    val icon = varchar("icon", 32)
+    val email = varchar("email", 20).nullable()
+    val school = varchar("school", 13)
+    val range = integer("range").default(10)
+}
