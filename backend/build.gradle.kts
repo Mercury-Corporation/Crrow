74d8e351ba633887b9035fa4f912a9e5198d005d
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 11:31:42 +0900

    feat(backend): add dependency exposed-java-time

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
