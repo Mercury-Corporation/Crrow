c717656df1b520fdeaab9c5bbf032204d6966af7
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 11:33:35 +0900

    feat(backend): update User.kt

--- backend/src/main/kotlin/com/ko610/models/User.kt
+++ backend/src/main/kotlin/com/ko610/models/User.kt
@@ -1,12 +1,15 @@
 package com.ko610.models
 
-data class User(
-    val id: Int,
-    val name: String,
-    val birthday: String,
-    val sex: Int,
-    val introduction: String,
-    val ban_level: Int,
-    val type: Int,
-    val coin: Int
-)
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.javatime.*
+
+object User: Table() {
+    val id = integer("id")
+    val name = varchar("name", 15)
+    val birthday = date("birthday")
+    val sex = integer("sex")
+    val introduction = varchar("introduction", 200)
+    val banLevel = integer("ban_level")
+    val type = integer("type")
+    val coin = integer("coin")
+}
