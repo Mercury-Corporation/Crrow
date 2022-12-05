50ea99bc7d292df57c9eb6622b73404b428a0149
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 13:03:32 +0900

    fix(backend): typo

--- backend/src/main/kotlin/com/ko610/models/Setting.kt
+++ backend/src/main/kotlin/com/ko610/models/Setting.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.javatime.date
 
 object Setting : Table("Setting") {
     val userId = reference("User_id", User)
-    val nickName = varchar("nickname", 20)
+    val nickname = varchar("nickname", 20)
     val icon = varchar("icon", 32)
     val email = varchar("email", 20).nullable()
     val school = varchar("school", 13)
