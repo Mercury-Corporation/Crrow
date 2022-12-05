c7b5cf5992fee33e098af87504d003d21eef058f
yuuki1293 <yuuki1293@outlook.jp>
2022-12-05 11:24:34 +0900

    feat(backend): call DatabaseFactory.init

--- backend/src/main/kotlin/com/ko610/Application.kt
+++ backend/src/main/kotlin/com/ko610/Application.kt
@@ -1,10 +1,14 @@
 package com.ko610
 
+import com.ko610.dao.DatabaseFactory
 import com.ko610.plugins.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 
-fun main(args: Array<String>) = EngineMain.main(args)
+fun main(args: Array<String>) = run {
+    DatabaseFactory.init()
+    EngineMain.main(args)
+}
 
 fun Application.module() {
     configureRouting()
